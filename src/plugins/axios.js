e6bbddfd21b567ea113d7614f679764171b86bff
azharadyatma <2210631170011@student.unsika.ac.id>
2024-12-02 01:43:57 +0700

    deploy frontend

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://inventory-management-backend-gules.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
